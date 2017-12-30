9e53b65a3211418ae749d63675427fc3350307a7
Andrewpqc <m13636038496@163.com>
2017-12-30 12:28:37 +0800

    finish 1035 lei

--- PAT/BasicLevel/1034.cpp
+++ PAT/BasicLevel/1034.cpp
@@ -1,11 +1,52 @@
 /**
- *有理数四则运算 这题好变态啊,我见过这世界上最恶心的一道题就是它了
+ *有理数四则运算 这题好变态啊,我见过这世界上最恶心的一道题就是它了,但是写出来成就感满满啊ls
  **/
 #include <iostream>
 #include <cctype>
 #include <cstdlib>
+using namespace std;
+
+struct Pair{
+    int a;
+    int b;
+};
+
+//输入两个数a,b给出a/b的最简单形式a'/b',返回a',b'
+//这里的前提条件是a>=1,b>=2,a<b,且a,b都为正整数
+struct Pair simplify_machine(int a,int b){
+    struct Pair P;
+    if(a==1||b==2){
+        P.a=a;
+        P.b=b;
+    }else{
+        //不断除以a,b的公约数，直到a,b互质
+        int temp=a;
+        for(int i=2;i<=temp;i++){
+            if(a%i==0 && b%i==0){
+                a=a/i;
+                b=b/i;
+                temp=a;
+                i=1;
+            }
+        }
+        P.a=a;
+        P.b=b;
+    }
+    return P;
+}
+
+
+
 // n分子，m分母,f正负　true为正
 void print(int n,int m,bool f){
+    if(n==0){
+        cout<<0<<" ";
+        return ;
+    }
+    if(m==0){
+        cout<<"Inf"<<endl;
+        return ;
+    }
     if(f){//正数
         if(n<m)
             cout<<n<<'/'<<m<<" ";
@@ -14,66 +55,97 @@ void print(int n,int m,bool f){
             if(!remainer){//余数为０
                 cout<<n/m<<" ";
             }else{//余数不为０
-                int nx=n-m*remainer;
-                cout<<n/m<<" "<<nx<<m<<" ";
+                struct Pair P=simplify_machine(remainer,m);
+                cout<<n/m<<" "<<P.a<<'/'<<P.b<<" ";
             }
         }
-    }else{//负数
-        if(n<m)
-            cout<<"(-"<<n<<'/'<<m<<")"<<" ";
+    }else if(!f){//负数
+        if(n<m){
+            struct Pair P=simplify_machine(n,m);
+            cout<<"(-"<<P.a<<'/'<<P.b<<")"<<" ";
+        }
         else if(n>=m){
             int remainer=n%m;
             if(!remainer){//余数为０
                 cout<<"(-"<<n/m<<")"<<" ";
             }else{//余数不为０
-                int nx=n-m*remainer;
-                cout<<"(-"<<n/m<<" "<<nx<<m<<")"<<" ";
+                struct Pair P=simplify_machine(remainer,m);
+                cout<<"(-"<<n/m<<" "<<P.a<<"/"<<P.b<<")"<<" ";
             }
         }
 
     }
 }
 
-void print+(int n1,int m1,int n2,int m2,bool f1,bool f2){
+void add(int n1,int m1,int n2,int m2,bool f1,bool f2){
     //n1*m2 n2*m1 m1*m2 f1 f2
     if(f1&&f2){//都为正
-        int sum=n1*m2+n2*m1;
-        int remainer=sum%(m1*m2);
-        if(!remainer)
-            cout<<sum/(m1*m2)<<endl;
-        else{//余数不为0
-            int t=sum-sum/(m1*m2)*m1*m2;
-            cout<<sum/(m1*m2)<<" "<<t<<"/"<<m1*m2<<endl;
-        }
+        print(n1*m2+n2*m1,m1*m2,true);
+        cout<<endl;
     }
     else if(!f1&&!f2){//都为负
-        int sum=n1*m2+n2*m1;
-        int remainer=sum%(m1*m2);
-        if(!remainer)
-            cout<<"(-"<<sum/(m1*m2)<<")"<<endl;
-        else{//余数不为0
-            int t=sum-sum/(m1*m2)*m1*m2;
-            cout<<"(-"<<sum/(m1*m2)<<" "<<t<<"/"<<m1*m2<<")"<<endl;
-        }
+        print(n1*m2+n2*m1,m1*m2,false);
+        cout<<endl;
     }
     else if(f1 && !f2){//1为＋,２为-
-        //可以转化为print-
+        int sum=n1*m2-n2*m1;
+        if(sum>=0){
+            print(sum,m1*m2,true);
+            cout<<endl;
+        }else{
+            print(-sum,m1*m2,false);
+            cout<<endl;
+        }
 
     }
     else if(!f1 && f2){//１为－，２为＋
-        
+        int sum=n2*m1-n1*m2;
+        if(sum>=0){
+            print(sum,m1*m2,true);
+            cout<<endl;
+        }else{
+            print(sum,m1*m2,false);
+            cout<<endl;
+        }
     }
 
 
 };
-void print-(int n1,int m1,int n2,int m2,bool f1,bool f2){
-
+void shorten(int n1,int m1,int n2,int m2,bool f1,bool f2){
+    // if(f1&&f2){//同为正
+    //     add(n1,m1,n2,m2,f1,!f2);
+    // }
+    // else if(!f1&&!f2){//同为负
+    //     add(n1,m1,n2,m2,f1,!f2);
+    // }
+    // else if(!f1&&f2){
+    //     add(n1,m1,n2,m2,f1,!f2)
+    // }
+    // else if(f1&&!f2){
+    //     add(n1,m1,n2,m2,f1,!f2);
+    // }
+    //写完上面这么多，才发现就等于下面这一句:(
+    add(n1,m1,n2,m2,f1,!f2);
 };
-void print*(int n1,int m1,int n2,int m2,bool f1,bool f2){
-
+void mult(int n1,int m1,int n2,int m2,bool f1,bool f2){
+    if((f1&&f2) || (!f1&&!f2)){
+        print(n1*n2,m1*m2,true);
+        cout<<endl;
+    }
+    else{
+        print(n1*n2,m1*m2,false);
+        cout<<endl;
+    }
 };
-void print/(int n1,int m1,int n2,int m2,bool f1,bool f2){
-
+void chu(int n1,int m1,int n2,int m2,bool f1,bool f2){
+    if((f1&&f2) || (!f1&&!f2)){
+        print(n1*m2,m1*n2,true);
+        cout<<endl;
+    }
+    else{
+        print(n1*m2,m1*n2,false);
+        cout<<endl;
+    }
 };
 
 
@@ -115,13 +187,30 @@ int main (void){
     int n12=atoi(s12.c_str());
     int n21=atoi(s21.c_str());
     int n22=atoi(s22.c_str());
+    //开始打印了
+    print(n11,n12,f1);
+    cout<<"+"<<" ";
+    print(n21,n22,f2);
+    cout<<"="<<" ";
+    add(n11,n12,n21,n22,f1,f2);
 
-
+    print(n11,n12,f1);
+    cout<<"-"<<" ";
+    print(n21,n22,f2);
+    cout<<"="<<" ";
+    shorten(n11,n12,n21,n22,f1,f2);
     
+    print(n11,n12,f1);
+    cout<<"*"<<" ";
+    print(n21,n22,f2);
+    cout<<"="<<" ";
+    mult(n11,n12,n21,n22,f1,f2);
 
-
-
-
+    print(n11,n12,f1);
+    cout<<"/"<<" ";
+    print(n21,n22,f2);
+    cout<<"="<<" ";
+    chu(n11,n12,n21,n22,f1,f2);
     return 0;
 }
 
