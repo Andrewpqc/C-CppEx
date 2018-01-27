998b4acce59b024837ffbae553a27ba3c45aeb80
Andrewpqc <m13636038496@163.com>
2018-01-27 17:02:44 +0800

    1067-1074

---
+++ PAT/BasicLevel/1074.cpp
@@ -0,0 +1,16 @@
+/**
+ * 宇宙无敌加法器
+ **/ 
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main (void){
+    string table,num1,num2;
+    cin>>table>>num1>>num2;
+    
+
+
+
+    return 0;
+}
